1aa6b519f7a20c168b738dd54f73f0cbe4a3dc8b
Web Team <web@dezrez.com>
2018-07-04 21:52:29 +0100

    Day 2 - Adding Geocoding

--- App/viewmodels/home.ts
+++ App/viewmodels/home.ts
@@ -3,28 +3,62 @@ import ICrime = require('interfaces/ICrime');
 
 class HomeView {
     title: string = 'Crime Tracking';
-    apiurl: string = 'https://stolenbikes88-datapoliceuk.p.mashape.com/crimes-at-location?date=2018-04&lat=51.66747&lng=-3.92846';
+    public apiUrl: KnockoutObservable<string>;
     public postcode: KnockoutObservable<string> = ko.observable('');
     public crimes: KnockoutObservableArray<ICrime> = ko.observableArray<ICrime>();
 
     public activate() {
 
+        this.apiUrl = ko.observable('https://stolenbikes88-datapoliceuk.p.mashape.com/crimes-at-location?date=2018-04');
 
         return true;
     }
 
-    public clickUpdateCrimes() {
-        var newThis = this;
+    public geocoderCallback(results: google.maps.GeocoderResult[], status: google.maps.GeocoderStatus) {
+
+        var _self = this;
+
+        if (results.length == 0) {
+            //TODO: Error, postcode not found
+            return;
+        }
+        var myResult = results[0];
+        var myLat = myResult.geometry.location.lat();
+        var myLng = myResult.geometry.location.lng();
+
+        var myApiUrl = this.apiUrl() + '&lat=' + myLat + '&lng=' + myLng;
         $.ajax({
             dataType: 'json',
-            url: this.apiurl,
+            url: myApiUrl,
             headers: {
                 "X-Mashape-Key": "zTWh1BDhG0msh6XIcvTMkh3dagw0p1mE7xDjsnS4MP3f1G3wHE"
             },
             success: function(data) {
-                newThis.crimes(data);
-                
+                _self.crimes(data);
+            },
+            error: function() {
+                //TODO: Error
             }
+        });
+    }
+
+    public clickUpdateCrimes() {
+
+        var _self = this;
+
+        //First get the latitude and longitude from the postcode entered
+        var myGeocoder = new google.maps.Geocoder();
+
+        var geocoderRequest = {
+            //TODO: Verify postcode
+            address: _self.postcode(),
+            componentRestrictions: {
+                country: 'GB'
+            }
+        }
+
+        myGeocoder.geocode(geocoderRequest, function(results: google.maps.GeocoderResult[], status: google.maps.GeocoderStatus) {
+            _self.geocoderCallback(results, status);
         });
     }
 }
