7baffca506a1eea69e691234266a65a22e524446
Web Team <web@dezrez.com>
2018-07-08 18:47:11 +0100

    Day 3 - Adding maps screen

--- App/viewmodels/home.ts
+++ App/viewmodels/home.ts
@@ -6,10 +6,12 @@ class HomeView {
     public apiUrl: KnockoutObservable<string>;
     public postcode: KnockoutObservable<string> = ko.observable('');
     public crimes: KnockoutObservableArray<ICrime> = ko.observableArray<ICrime>();
+    public map: google.maps.Map;
 
     public activate() {
 
         this.apiUrl = ko.observable('https://stolenbikes88-datapoliceuk.p.mashape.com/crimes-at-location?date=2018-04');
+        
 
         return true;
     }
@@ -26,7 +28,7 @@ class HomeView {
         var myLat = myResult.geometry.location.lat();
         var myLng = myResult.geometry.location.lng();
 
-        var myApiUrl = this.apiUrl() + '&lat=' + myLat + '&lng=' + myLng;
+        var myApiUrl = _self.apiUrl() + '&lat=' + myLat + '&lng=' + myLng;
         $.ajax({
             dataType: 'json',
             url: myApiUrl,
@@ -34,10 +36,26 @@ class HomeView {
                 "X-Mashape-Key": "zTWh1BDhG0msh6XIcvTMkh3dagw0p1mE7xDjsnS4MP3f1G3wHE"
             },
             success: function(data) {
-                _self.crimes(data);
+
+                m_logger.logger.log(data.length + ' crime(s) returned.', null, 'home', true);
+
+                _self.map = new google.maps.Map(document.getElementById('map'), { center: { lat: myLat, lng: myLng }, zoom: 17 });
+
+                $.each(data, function(_, crime) {
+                    var marker = new google.maps.Marker({position: { lat: parseFloat(crime.location.latitude), lng: parseFloat(crime.location.longitude) }, map: _self.map });
+                    var windowcontent = 'Category: ' + crime.category + '<br/>';
+                    if (crime.outcome_status) {
+                        windowcontent += 'Outcome Status: ' + crime.outcome_status.category;
+                    }
+
+                    var infowindow = new google.maps.InfoWindow({content: windowcontent});
+                    marker.addListener('click', function() {
+                        infowindow.open(_self.map, marker);
+                    })
+                })
             },
             error: function() {
-                //TODO: Error
+                m_logger.logger.log('Error when requesting requesting crime data.', null, 'home', true);
             }
         });
     }
@@ -45,6 +63,8 @@ class HomeView {
     public clickUpdateCrimes() {
 
         var _self = this;
+
+        m_logger.logger.log('Requesting crimes.', null, 'home', true);
 
         //First get the latitude and longitude from the postcode entered
         var myGeocoder = new google.maps.Geocoder();
